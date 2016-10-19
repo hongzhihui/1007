883a1073754b53c3b31173e1716383f49933fcc0
josehp <hong7127@163.com>
2016-10-19 20:44:54 +0800

    添加了嘻嘻注释

---
+++ src/demo1/User.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package demo1;
+
+/**
+ * @author Josehp
+ *
+ */
+public class User {
+
+	/**
+	 * @param args
+	 */
+	
+
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		 Vehicle car1,car2;    
+	     car1=new Vehicle(); //使用new 运算符和默认的构造方法创建对象car1
+	      car2=new Vehicle(); //使用new 运算符和默认的构造方法创建对象car2
+	      car1.setPower(128);   
+	      car2.setPower(76);
+	      System.out.println("car1的功率是："+car1.getPower());
+	      System.out.println("car2的功率是："+car2.getPower());
+	      car1.speedUp(80);  //car1调用speedUp方法将自己的speed的值增加80
+	      car2.speedUp(100); //car2调用speedUp方法将自己的speed的值增加80
+	      System.out.println("car1目前的速度："+car1.getSpeed());
+	      System.out.println("car2目前的速度："+car2.getSpeed());
+	      car1.speedDown(10);
+	      car2.speedDown(20);
+	      System.out.println("car1目前的速度："+car1.getSpeed());
+	      System.out.println("car2目前的速度："+car2.getSpeed());
+		  //xixi
+		  //版本1
+	}
+
+}
---
+++ src/demo1/Vehicle.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package demo1;
+
+/**
+ * @author Josehp
+ *
+ */
+public class Vehicle {
+    double speed;//声明double型变量speed,刻画速度
+    int power;//声明int型变量power,刻画功率
+    void speedUp(int s) {  
+       speed=s+speed;   //将参数s的值与成员变量speed的和赋值给成员变量speed
+       if(speed>200)
+       {  System.out.println("error");
+       speed=speed-s;}
+    } 
+    void speedDown(int d) {   
+       speed=speed-d; //将成员变量speed与参数d的差赋值给成员变量speed
+       if(speed<0)
+       {  System.out.println("error");
+       speed=speed+d;}
+    } 
+    void setPower(int p) {
+       power=p;//将参数p的值赋值给成员变量power
+    }
+    void brake(){
+    	speed=0;
+    }
+    int getPower() {
+       return power; //返回成员变量power的值
+    }
+    double getSpeed() {
+       return speed;
+    }
+}
